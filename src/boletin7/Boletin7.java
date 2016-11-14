dc6bfbe2fec01bd6a473b1795e81aeed16e04d6f
antonio <antonio@biruseto>
2016-11-14 12:38:29 +0100

    boletin 7 main

---
+++ src/boletin7/Boletin7.java
@@ -0,0 +1,16 @@
+
+package boletin7;
+
+import javax.swing.JOptionPane;
+
+
+public class Boletin7 {
+     public static void main(String arg[]){
+         
+    Libro obxlib1= new Libro("el codigo da Vinci", "Dan Brown", -91,1);
+    obxlib1.comprobar(obxlib1.prestamo());
+    obxlib1.comprobar(obxlib1.prestamo());
+    
+    
+     }
+}
